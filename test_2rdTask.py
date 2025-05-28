cd96c3f9e77d14a96f0c154eb425d591f19c2f51
F-L-C-L <fckthss@gmail.com>
2025-05-28 15:20:13 +0300

    Fin

--- test_2rdTask.py
+++ test_2rdTask.py
@@ -1,16 +1,34 @@
 import pytest
-from playwright.sync_api import expect
-EXPECTED_TITLE = "Fast and reliable end-to-end testing for modern web apps | Playwright"
-LINK = "https://playwright.dev/"
+from playwright.sync_api import expect, Playwright
+
+
+EXPECTED_TITLE: str = "Fast and reliable end-to-end testing for modern web apps | Playwright"
+LINK: str = "https://playwright.dev/"
+
+
 @pytest.mark.parametrize("browser_type", ["chromium", "firefox"])
 def test_page_title(browser_type, playwright):
+    """
+    Verifies that the page title matches the expected value for the specified browser type.
+
+    Args:
+        browser_type (str): The type of browser to launch ("chromium" or "firefox").
+        playwright (Playwright): A Playwright instance used to launch browser instances.
+
+    Raises:
+        pytest.fail: If an unsupported browser type is provided.
+
+    Usage Example:
+        test_page_title("chromium", playwright)
+    """
     if browser_type == "chromium":
         browser = playwright.chromium.launch()
     elif browser_type == "firefox":
         browser = playwright.firefox.launch()
     else:
         pytest.fail(f"Unsupported browser type: {browser_type}")
-    # Создаем контекст и страницу
+    
+	# Create a new context and page
     context = browser.new_context()
     page = context.new_page()
     try:
